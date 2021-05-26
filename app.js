d769c17aa60141707a4ad9029b4190cce0dd63b5
Kakamotobi <79886384+Kakamotobi@users.noreply.github.com>
2021-05-26 19:04:28 +0900

    Implement results recording feature

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const guessHelper = document.querySelector("#guess-helper");
 const resultsTable = document.querySelector(".results-table");
 let randNum;
 let attempts = 0;
+let round = 0;
 
 // ----------Generate Random Number---------- //
 randNumGenBtn.addEventListener("click", () => {
@@ -23,6 +24,8 @@ guessBtn.addEventListener("click", () => {
     attempts++;
     if (parseInt(guessInput.value) === randNum) {
         randNumDisplay.innerText = randNum;
+        round++;
+        recordResult();
         guessInput.value = "";
         guessHelper.innerText = `Congrats! It took you ${attempts} ${guessPlurality(
             attempts
@@ -62,4 +65,18 @@ function guessPlurality(attempts) {
     } else {
         return "guess";
     }
+}
+// -----Results Table----- //
+function recordResult() {
+    const tableRow = document.createElement("tr");
+    const tableRoundNum = document.createElement("td");
+    const tableAnswer = document.createElement("td");
+    const tableGuesses = document.createElement("td");
+    resultsTable.append(tableRow);
+    tableRow.append(tableRoundNum, tableAnswer, tableGuesses);
+    tableRoundNum.append(round);
+    tableAnswer.append(
+        `${randNumDisplay.innerText} (${minInput.value} - ${maxInput.value})`
+    );
+    tableGuesses.append(`${attempts} ${guessPlurality(attempts)}`);
 }
