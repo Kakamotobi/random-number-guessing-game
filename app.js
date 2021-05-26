b78fed11820aa2104206d7b95cf3ab2c34df3467
Kakamotobi <79886384+Kakamotobi@users.noreply.github.com>
2021-05-26 18:58:38 +0900

    Implement guess feedback

--- app.js
+++ app.js
@@ -6,11 +6,39 @@ const guessInput = document.querySelector("#guess-input");
 const guessBtn = document.querySelector("#guess-button");
 const guessHelper = document.querySelector("#guess-helper");
 const resultsTable = document.querySelector(".results-table");
+let randNum;
+let attempts = 0;
 
 // ----------Generate Random Number---------- //
 randNumGenBtn.addEventListener("click", () => {
     randNumGenerator(minInput, maxInput);
     randNumDisplay.innerHTML = "?";
+    guessHelper.classList.remove("correct");
+    guessHelper.classList.remove("incorrect");
+    guessHelper.innerText = "Good Luck!";
+});
+
+// ----------Guess---------- //
+guessBtn.addEventListener("click", () => {
+    attempts++;
+    if (parseInt(guessInput.value) === randNum) {
+        randNumDisplay.innerText = randNum;
+        guessInput.value = "";
+        guessHelper.innerText = `Congrats! It took you ${attempts} ${guessPlurality(
+            attempts
+        )}!`;
+        guessHelper.classList.remove("incorrect");
+        guessHelper.classList.add("correct");
+        attempts = 0;
+    } else if (parseInt(guessInput.value) < parseInt(minInput.value)) {
+        incorrect(`Your minimum is set to ${minInput.value}!`);
+    } else if (parseInt(guessInput.value) > parseInt(maxInput.value)) {
+        incorrect(`Your maximum is set to ${maxInput.value}!`);
+    } else if (parseInt(guessInput.value) > randNum) {
+        incorrect("Too High! Enter a new guess!");
+    } else if (parseInt(guessInput.value) < randNum) {
+        incorrect("Too Low! Enter a new guess!");
+    }
 });
 
 // ----------Functions---------- //
@@ -21,4 +49,17 @@ function randNumGenerator(minInput, maxInput) {
             parseInt(Math.random() * (maxInput.value - minInput.value + 1))
     );
     return randNum;
+}
+// -----Incorrect----- //
+function incorrect(helperMsg) {
+    guessHelper.classList.add("incorrect");
+    guessHelper.innerText = `${helperMsg}`;
+}
+// -----Guess Plurality----- //
+function guessPlurality(attempts) {
+    if (attempts > 1) {
+        return "guesses";
+    } else {
+        return "guess";
+    }
 }
